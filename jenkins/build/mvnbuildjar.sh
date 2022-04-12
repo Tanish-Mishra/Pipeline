e31ca422f68d82f1c983286b0b7ea81d140b8af1
Tanish Mishra <theheronzclub@gmail.com>
2022-04-12 13:27:30 +0530

    This will work

--- jenkins/build/mvnbuildjar.sh
+++ jenkins/build/mvnbuildjar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "*************************************"
 echo "**********Building the Jar***********"
-WORKDIR=/cicd-app/jenkins_home/workspace/maven-project
+WORKDIR=/cicd-app/jenkins_home/workspace/cicd
 docker run --rm -v $WORKDIR/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3.5.4-alpine "$@"
--- jenkins/test/mvntest.sh
+++ jenkins/test/mvntest.sh
@@ -1,5 +1,5 @@
 echo "***************************"
 echo "**********Testing**********"
 echo "***************************"
-WORKDIR=/cicd-app/jenkins_home/workspace/pipeline-maven-deployment
+WORKDIR=/cicd-app/jenkins_home/workspace/cicd
 docker run --rm -v $WORKDIR/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5.4-alpine "$@"
