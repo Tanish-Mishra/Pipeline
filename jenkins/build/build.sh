1a8e6302bebab06df8a26899615c38737339191e
Tanish Mishra <theheronzclub@gmail.com>
2022-04-12 12:30:13 +0530

    newop

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -3,4 +3,4 @@ cp -f /pipeline/java-app/target/*.jar /pipeline/jenkins/build
 echo "********************************************************************"
 echo "********************Building The Image******************************"
 echo "*********************************************************************"
-cd /cicd-app/jenkins_home/workspace/pipeline-maven-deployment/jenkins/build && docker-compose -f docker-compose-build-javaapp.yaml build --no-cache
+cd jenkins/build && docker-compose -f docker-compose-build-javaapp.yaml build --no-cache
